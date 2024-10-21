3ad33a2e5334c10ad34d4346c849f6d03a88a5cd
jmiraf <jenmmiraf@gmail.com>
2024-10-21 16:51:18 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CodeFirstStudentDatabase
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SchoolContext())
+            {
+                //Add a new student
+                Console.WriteLine("Enter Student's fullname:");
+                var name = Console.ReadLine();
+
+                var stud = new Student { StudentName = name };
+                db.Students.Add(stud);
+                db.SaveChanges();
+
+                Console.WriteLine("Student saved successfully!");
+
+                //Display all students from the database
+                var query = from s in db.Students orderby s.StudentName select s;
+
+                Console.WriteLine("All Students in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.StudentName);
+                }
+                Console.WriteLine("Press ESC to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public Grade Grade { get; set; }
+
+    }
+    public class Grade
+    {
+        public int GradeID { get; set; }
+        public string Section { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+
+    public class SchoolContext: DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CodeFirstStudentDatabase")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Oracle Corporation")]
+[assembly: AssemblyProduct("CodeFirstStudentDatabase")]
+[assembly: AssemblyCopyright("Copyright © Oracle Corporation 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("94e69da3-a051-41d6-81e3-953b2b9067b8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
